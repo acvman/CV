7b6ef8be3a4625961d2d35eecadd7d889d5ec88a
acvman <xmutcv@163.com>
2024-11-10 22:35:17 +0800

    更新cmakelist 支持OpenCV

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include "../include/add.h"
+#include <opencv2/opencv.hpp>
+int main() {
+    // 读取图像
+    cv::Mat image = cv::imread("/Users/zwg/CLionProjects/untitled4/WechatIMG1055.jpeg");
+    if (image.empty()) {
+        std::cerr << "Error: Could not load image!" << std::endl;
+        return -1;
+    }
 
-int main()
-{
-    int re= my_add(1,3);
-    std::cout<<"re= "<<re<<std::endl;
+    // 显示图像
+    cv::imshow("Display Image", image);
+    cv::waitKey(0); // 等待按键事件
+
     return 0;
 }
+
